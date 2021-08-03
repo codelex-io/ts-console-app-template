726e7b011ec086fcd54f134614b92115755625e7
sandris <a.sandris@gmail.com>
2021-08-03 15:24:29 +0300

    tsconfig and export explainer

--- src/echo.ts
+++ src/echo.ts
@@ -9,3 +9,10 @@ import * as prompts from "prompts";
 
   console.log(`Hi, ${response.name}! Nice to see you here 😊`);
 })();
+
+/**
+ * You may run into a problem like "Cannot redeclare block scoped variable name"
+ * Each exercise that you are solving in a new file must have an export present
+ * Read more here - https://backbencher.dev/articles/typescript-solved-cannot-redeclare-block-scoped-variable-name
+ */
+export {};
